7f46d61cc49f717e035f0ff4679c67bcb99fa92b
mtmarctoni <marctonimas@protonmail.com>
2024-12-31 13:04:54 +0100

    'update'

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,10 +6,20 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3003',
+      '/': {
+        target: 'https://blog-list-app.netlify.app',
         changeOrigin: true,
       },
     },
   },
 })
+
+//dev mode 
+/*
+proxy: {
+      '/api': {
+        target: 'http://localhost:3003',
+        changeOrigin: true,
+      },
+    },
+*/
